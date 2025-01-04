b69c0f33a797574784b3186bd244f6bf2a5fb636
Xayrullo <xayrullo936@gmail.com>
2025-01-04 13:08:09 -0500

    add new file

--- src/views/ServicesPage.vue
+++ src/views/ServicesPage.vue
@@ -20,110 +20,129 @@ export default {
             <h1>Наша Услуги</h1>
         </div>
         <hr />
-        <div class="row">
-            <div class="printer">
-                <div class="content">
-                    <h3>РЕМОНТ МФУ</h3>
-                    <h4>принтеров</h4>
-                    <p>Все бренды</p>
+        <div data-aos="fade-up"
+             data-aos-anchor-placement="bottom-bottom">
+            <div class="row">
+                <div class="printer">
+                    <div class="content">
+                        <h3>РЕМОНТ МФУ</h3>
+                        <h4>принтеров</h4>
+                        <p>Все бренды</p>
+                    </div>
+                    <img alt="img" class="hover-image" src="@/ServiceImage/img.png">
+                    <button class="btn" @click="PrintList = true">Подробнее</button>
+                    <div v-if="PrintList" class="list">
+                        <button class="close-btn" @click="PrintList = false">X</button>
+                        <h3 class="information">Детали</h3>
+                        <p>Ремонт МФУ принтеров всех брендов. Замена картриджей, ремонт печатающей головки, восстановление
+                            фотобарабана, настройка сетей.</p>
+                    </div>
                 </div>
-                <img alt="img" class="hover-image" src="@/ServiceImage/img.png">
-                <button class="btn" @click="PrintList = true">Подробнее</button>
-                <div v-if="PrintList" class="list">
-                    <button class="close-btn" @click="PrintList = false">X</button>
-                    <h3 class="information">Детали</h3>
-                    <p>Ремонт МФУ принтеров всех брендов. Замена картриджей, ремонт печатающей головки, восстановление
-                        фотобарабана, настройка сетей.</p>
+                <div class="notebook">
+                    <div class="content">
+                        <h3>РЕМОНТ</h3>
+                        <h4>Notebook</h4>
+                        <p>Ремонт ноутбуков всех марок и моделей</p>
+                    </div>
+                    <img alt="img" class="hover-image" src="@/ServiceImage/img_1.png">
+                    <button class="btn" @click="NotebookList = true">Подробнее</button>
+                    <div v-if="NotebookList" class="list">
+                        <button class="close-btn" @click="NotebookList = false">X</button>
+                        <h3 class="information">Детали</h3>
+                        <p>Профессиональный ремонт ноутбуков. Замена матрицы, клавиатуры, жесткого диска, ремонт материнской
+                            платы и многое другое. Быстрая и надежная диагностика.</p>
+                    </div>
+                </div>
+                <div class="shreder">
+                    <div class="content">
+                        <h3>РЕМОНТ</h3>
+                        <h4>Шредеров</h4>
+                        <p>Решение проблемы. Ремонт и обслуживание шредеров</p>
+                    </div>
+                    <img alt="img" class="hover-image" src="@/ServiceImage/img_2.png">
+                    <button class="btn" @click="ShredrovList = true">Подробнее</button>
+                    <div v-if="ShredrovList" class="list">
+                        <button class="close-btn" @click="ShredrovList = false">X</button>
+                        <h3 class="information">Детали</h3>
+                        <p>Ремонт и техническое обслуживание шредеров различных производителей. Быстрое восстановление
+                            работоспособности вашего оборудования. Замена изношенных деталей.</p>
+                    </div>
                 </div>
             </div>
-            <div class="notebook">
-                <div class="content">
-                    <h3>РЕМОНТ</h3>
-                    <h4>Notebook</h4>
-                    <p>Все бренды</p>
+            <div class="row">
+                <div class="termo_print">
+                    <div class="content">
+                        <h3>РЕМОНТ МФУ</h3>
+                        <h4>Термопринт</h4>
+                        <p>С гарантией</p>
+                    </div>
+                    <img alt="img" class="hover-image" src="@/ServiceImage/img_3.png">
+                    <button class="btn" @click="TermoPrintList = true">Подробнее</button>
+                    <div v-if="TermoPrintList" class="list">
+                        <button class="close-btn" @click="TermoPrintList = false">X</button>
+                        <h3 class="information">Детали</h3>
+                        <p>Качественный ремонт термопринтеров с гарантией. Замена термоэлемента, роликов, ремонт
+                            электроники, настройка и калибровка.</p>
+                    </div>
                 </div>
-                <img alt="img" class="hover-image" src="@/ServiceImage/img_1.png">
-                <button class="btn" @click="NotebookList = true">Подробнее</button>
-                <div v-if="NotebookList" class="list">
-                    <button class="close-btn" @click="NotebookList = false">X</button>
-                    <h3 class="information">Детали</h3>
-                    <p>Профессиональный ремонт ноутбуков. Замена матрицы, клавиатуры, жесткого диска, ремонт материнской
-                        платы и многое другое. Быстрая и надежная диагностика.</p>
+                <div class="plotterov">
+                    <div class="content">
+                        <h3>РЕМОНТ</h3>
+                        <h4>Плоттеров</h4>
+                        <p>Любая сложность</p>
+                    </div>
+                    <img alt="img" class="hover-image" src="@/ServiceImage/img_4.png">
+                    <button class="btn" @click="PlotterovList = true">Подробнее</button>
+                    <div v-if="PlotterovList" class="list">
+                        <button class="close-btn" @click="PlotterovList = false">X</button>
+                        <h3 class="information">Детали</h3>
+                        <p>Ремонт плоттеров любой сложности. Замена чернильных голов, ремонт механизмов подачи бумаги,
+                            восстановление электронных плат.</p>
+                    </div>
                 </div>
-            </div>
-            <div class="shreder">
-                <div class="content">
-                    <h3>РЕМОНТ</h3>
-                    <h4>Шредеров</h4>
-                    <p>Все бренды</p>
-                </div>
-                <img alt="img" class="hover-image" src="@/ServiceImage/img_2.png">
-                <button class="btn" @click="ShredrovList = true">Подробнее</button>
-                <div v-if="ShredrovList" class="list">
-                    <button class="close-btn" @click="ShredrovList = false">X</button>
-                    <h3 class="information">Детали</h3>
-                    <p>Ремонт и техническое обслуживание шредеров различных производителей. Быстрое восстановление
-                        работоспособности вашего оборудования. Замена изношенных деталей.</p>
+                <div class="kompyuter">
+                    <div class="content">
+                        <h3>РЕМОНТ</h3>
+                        <h4>КОМПЬЮТЕРОВ</h4>
+                        <p>Установка Windows программы.</p>
+                    </div>
+                    <img alt="img" class="hover-image" src="@/ServiceImage/img_5.png">
+                    <button class="btn" @click="KompyuterList = true">Подробнее</button>
+                    <div v-if="KompyuterList" class="list">
+                        <button class="close-btn" @click="KompyuterList = false">X</button>
+                        <h3 class="information">Детали</h3>
+                        <p>Опытные специалисты устранят любые неполадки вашего компьютера, от программных сбоев до
+                            аппаратных поломок. Гарантия на выполненные работы.</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="termo_print">
-                <div class="content">
-                    <h3>РЕМОНТ МФУ</h3>
-                    <h4>Термопринт</h4>
-                    <p>Все бренды</p>
-                </div>
-                <img alt="img" class="hover-image" src="@/ServiceImage/img_3.png">
-                <button class="btn" @click="TermoPrintList = true">Подробнее</button>
-                <div v-if="TermoPrintList" class="list">
-                    <button class="close-btn" @click="TermoPrintList = false">X</button>
-                    <h3 class="information">Детали</h3>
-                    <p>Качественный ремонт термопринтеров с гарантией. Замена термоэлемента, роликов, ремонт
-                        электроники, настройка и калибровка.</p>
-                </div>
-            </div>
-            <div class="plotterov">
-                <div class="content">
-                    <h3>РЕМОНТ</h3>
-                    <h4>Плоттеров</h4>
-                    <p>Все бренды</p>
-                </div>
-                <img alt="img" class="hover-image" src="@/ServiceImage/img_4.png">
-                <button class="btn" @click="PlotterovList = true">Подробнее</button>
-                <div v-if="PlotterovList" class="list">
-                    <button class="close-btn" @click="PlotterovList = false">X</button>
-                    <h3 class="information">Детали</h3>
-                    <p>Ремонт плоттеров любой сложности. Замена чернильных голов, ремонт механизмов подачи бумаги,
-                        восстановление электронных плат.</p>
-                </div>
-            </div>
-            <div class="kompyuter">
-                <div class="content">
-                    <h3>РЕМОНТ</h3>
-                    <h4>КОМПЬЮТЕРОВ</h4>
-                    <p>Все бренды</p>
-                </div>
-                <img alt="img" class="hover-image" src="@/ServiceImage/img_5.png">
-                <button class="btn" @click="KompyuterList = true">Подробнее</button>
-                <div v-if="KompyuterList" class="list">
-                    <button class="close-btn" @click="KompyuterList = false">X</button>
-                    <h3 class="information">Детали</h3>
-                    <p>Опытные специалисты устранят любые неполадки вашего компьютера, от программных сбоев до
-                        аппаратных поломок. Гарантия на выполненные работы.</p>
-                </div>
-            </div>
-        </div>
+
     </div>
 </template>
 
 <style scoped>
+[data-aos="fade-up"] {
+    transition-duration: 3s !important; /* "fade-up" animatsiyasi uchun davomiylik */
+}
+/* Umumiy konteyner uslubi */
+.container {
+    width: 90%; /* Konteyner kengligi */
+    max-width: 1200px; /* Maksimal kenglik */
+    margin: 0 auto; /* Markazlash */
+    padding: 0 15px; /* Ichki bo'shliq */
+    box-sizing: border-box; /* Paddingni hisoblash */
+}
+
+/* Sarlavha uslubi */
 .title {
     color: white;
     font-weight: bold;
-    text-align: center;
+    text-align: center; /* Matnni markazlash */
+    margin-bottom: 20px; /* Sarlavha ostidagi bo'sh joy */
 }
 
+/* Bo'linish chizig'i */
 hr {
     border: none;
     border-top: 2px solid gray;
@@ -131,25 +150,92 @@ hr {
     margin: 10px auto;
 }
 
-/* Ramka uslubi */
-.printer, .notebook, .shreder, .termo_print, .plotterov, .kompyuter {
-    border: 2px solid white;
-    border-radius: 10px;
-    overflow: hidden; /* Ramkadan tashqaridagi qism ko'rinmasin */
-    margin: 10px;
-    position: relative;
+.row {
+    display: flex; /* Yonma-yon joylashuv */
+    justify-content: space-between; /* Ramkalar orasidagi teng masofa */
+    flex-wrap: wrap; /* Qatorni o'ramli qilish */
+    gap: 10px; /* Elementlar orasidagi bo'shliq */
+    margin: 0 auto; /* Markazlash */
 }
+
+/* Ramkalar kengligi va balandligi */
+.printer, .notebook, .shreder, .termo_print, .plotterov, .kompyuter {
+    height: 250px; /* Ramkaning bir xil balandligi */
+    border: 2px solid white; /* Ramka */
+    border-radius: 10px; /* Yumaloqlik */
+    overflow: hidden; /* Ortiqcha qismni yashirish */
+    position: relative; /* Elementni joylashni aniqlash */
+    background-color: transparent;
+    margin-top: 20px; /* Tepadan bo'sh joy */
+    margin-bottom: 20px;
+}
+
+/* Kichik ekranlar uchun media-so'rov */
+@media (max-width: 768px) {
+    .row {
+        flex-wrap: wrap; /* Kichik ekranlarda ustun tartibi */
+        justify-content: center; /* Elementlarni markazlash */
+    }
+
+    .printer, .notebook, .shreder, .termo_print, .plotterov, .kompyuter {
+        flex: 0 0 100%; /* Kichik ekranlarda to'liq kenglik */
+        max-width: 100%; /* Maksimal kenglikni moslash */
+    }
+}
+
 
 .content {
-    position: absolute;
-    bottom: 40px; /* Pastdan 40px joy */
-    left: 15px; /* Chapdan 15px joy */
+    position: absolute; /* Matn joylashuvini sozlash */
+    bottom: 60px; /* Matn tugmadan yuqoriroq joylashadi */
+    left: 15px;
+    color: white;
 }
 
-h3, h4, p {
-    margin: 0; /* Ichki bo'shliqni olib tashlash */
-    color: white; /* Matn rangini oq qilib belgilash */
+p {
+    margin: 0;
+    color: white;
+    font-size: 14px; /* Matnning o‘lchamini kichikroq qilish */
+    line-height: 1.4;
 }
+
+h3 {
+    margin: 0;
+    color: white;
+    font-size: 18px; /* H3 elementining kattaligini oshirish */
+    font-weight: bold; /* Qalinroq matn uchun */
+}
+
+h4 {
+    margin: 0;
+    color: white;
+    font-size: 16px; /* H4 elementining kattaligini oshirish */
+    font-weight: bold; /* Qalinroq matn uchun */
+}
+
+
+.btn {
+    position: absolute;
+    right: 15px;
+    bottom: 20px; /* Tugmani pastroqqa qaytarish */
+    background-color: #282c34;
+    color: white;
+    font-weight: bold;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    padding: 10px 20px;
+    font-size: 14px;
+    line-height: 1;
+    white-space: nowrap;
+}
+
+.btn:hover {
+    background: #3c3e50;
+    color: white;
+}
+
+
+
 
 h3, h4 {
     display: inline;
@@ -159,83 +245,30 @@ h3, h4 {
     text-align: right;
 }
 
-.btn:hover {
-    background: #3c3e50;
-    color: white;
-}
-
-.btn {
-    position: absolute;
-    right: 15px; /* O'ngdan 15px bo'sh joy */
-    bottom: 15px; /* Pastdan 15px bo'sh joy */
-    background-color: #282c34;
-    color: white;
-    font-weight: bold;
-    border: none;
-    border-radius: 20px; /* Tugma uchun yumaloqlik */
-    cursor: pointer;
-    padding: 10px 20px; /* Tugma ichidagi matn uchun bo'sh joy */
-    font-size: 16px; /* Matn o'lchami */
-    line-height: 1; /* Matnni vertikal markazlash */
-    box-sizing: border-box; /* Padding va borderlarni hisobga olish */
-    white-space: nowrap; /* Matnni bir qator ichida ushlab turish */
-}
-
-.printer {
-    width: 512px;
-    height: 250px;
-}
-
-.notebook {
-    width: 284px;
-    height: 250px;
-}
-
-.shreder {
-    width: 340px;
-    height: 250px;
-}
-
-.termo_print {
-    width: 284px;
-    height: 250px;
-}
-
-.plotterov {
-    width: 570px;
-    height: 250px;
-}
-
-.kompyuter {
-    width: 284px;
-    height: 250px;
-}
-
 /* Tasvir uslubi */
 .hover-image {
-    position: absolute; /* Tasvirni ramkaga joylash */
+    position: absolute;
     top: 0;
     left: 0;
-    width: 100%; /* Tasvir ramkani to'liq egallaydi */
-    height: 100%; /* Tasvir ramkani to'liq egallaydi */
-    object-fit: cover; /* Tasvirni ramka ichiga moslashtirish */
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
     transition: transform 0.3s ease, box-shadow 0.3s ease;
 }
 
-/* Hover faqat tasvirda ishlashi uchun */
 .hover-image:hover {
-    transform: scale(1.1); /* Tasvir kattalashadi */
-    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.3);
+    transform: scale(1.1); /* Kattalashish */
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.3); /* Soha */
 }
 
-/* Qo'shimcha list uchun uslublar */
+/* Qo'shimcha matn bloki uslubi */
 .list {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.8); /* Shaffof qora fon */
+    background: rgba(0, 0, 0, 0.8); /* Qora fon */
     color: white;
     display: flex;
     flex-direction: column;
@@ -243,10 +276,10 @@ h3, h4 {
     align-items: center;
     padding: 20px;
     box-sizing: border-box;
-    z-index: 10; /* Ramka ichidagi boshqa elementlar ustida */
+    z-index: 10;
 }
 
-/* Listni yopish tugmasi */
+/* Yopish tugmasi */
 .close-btn {
     position: absolute;
     top: 10px;
@@ -260,6 +293,27 @@ h3, h4 {
 }
 
 .close-btn:hover {
-    color: #f00; /* Tugmani hover holatida qizil qilish */
+    color: #f00;
+}
+
+/* Ramkalar uchun o'lchamlar */
+.printer {
+    width: 512px;
+    height: 250px;
+}
+
+.notebook, .termo_print, .kompyuter {
+    width: 284px;
+    height: 250px;
+}
+
+.shreder {
+    width: 340px;
+    height: 250px;
+}
+
+.plotterov {
+    width: 570px;
+    height: 250px;
 }
 </style>
